27b8f8365400179e7a4649e410baaaeb7a87f376
Nathan <nathanlserrano@gmail.com>
2019-03-13 10:27:47 -0700

    Added login and sign up buttons to navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,12 +5,29 @@ import "./style.css";
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      navbar to edit...
-      <button>Loginbutton yay</button>
-      <button>Signupbutton yay</button>
+    <nav>
+      <div className="nav-wrapper">
+        <Link className="brand-logo" to="/">
+          FitMii
+        </Link>
+
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li>
+            <Link to="/" className="waves-effect waves-light btn">
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link to="/signup" className="waves-effect waves-light btn">
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
+
+// have a state that says "logged in" and pass it down as a prop
 
 export default Navbar;
